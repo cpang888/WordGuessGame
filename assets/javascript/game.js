1feac621cf4ca5a576243c84be71f7696423d667
Cindy Pang <cipang@coca-cola.com>
2018-06-03 09:10:55 -0400

    added comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,16 +5,12 @@
       {name: "canada", image: "canada.PNG", sound: "canada.mp3"},
       {name: "china", image: "china.PNG", sound: "china.mp3"}
     ];
-    
-    var image;
-    var countryAnthem;
-    var index;
 
     var game = {
       guessed: [],
       left: 10,
       reset: function () {
-        
+        // reset the selector 'right', 'left', 'remain, and 'wrong'
         this.$right.innerHTML = '';
         this.left = 10;
         this.$remain.innerHTML = this.left;
@@ -22,34 +18,51 @@
       },
       start: function() {
         this.complete = false;
-        index = Math.floor(Math.random() * words.length);
-        this.word = words[index].name;
-        image = words[index].image;
-        countryAnthem = words[index].sound;
+        // pick a random number between 0 to the length of the words
+        this.index = Math.floor(Math.random() * words.length);
+        // get the country name from the words object
+        this.word = words[this.index].name;
+        // get the image name from the words object
+        this.image = words[this.index].image;
+        // get the country anthem from the words object
+        this.countryAnthem = words[this.index].sound;
+        // get the 'right' selector. Returns a set of elements found in the DOM 'right'
         this.$right = document.getElementById('right');
+        // get the 'right' selector. Returns a set of elements found in the DOM 'wrong'
         this.$wrong = document.getElementById('wrong');
+        // get the 'right' selector. Returns a set of elements found in the DOM 'remain'
         this.$remain = document.getElementById('remain');
+        // display '_' for each character in the word
         this.$right.innerHTML = '_'.repeat(this.word.length);
       },
       guess: function(letter) {
+        // player press a key 'letter' from the keyboard
         if (this.left > 0 && this.complete != true) {
+          // remaining guess is greater than 0 and the game is not completed
+          // check the key input is it part of the 'word' or in the already guessed array
           if (this.word.indexOf(letter) > -1 || this.guessed.indexOf(letter) > -1) {
+            // player guessed the letter right. Go to the right function
             this.right(letter);
           } else {
+            // player guessed it wrong. Go to the wrong function
             this.wrong(letter);
           }
         }
       },
       right: function(letter) {
+        // for each letter in the word
         for(var i = 0; i < this.word.length; i++) {
+          // compare it with user input 'letter'
           if (this.word[i] == letter) {
+            // if equal, assign the letter to the new local variable word
             var word = this.$right.innerHTML.split('');
             word[i] = letter;
             this.$right.innerHTML = word.join('');
           }
         }
         if (this.$right.innerHTML.indexOf('_') < 0) {
-
+          // if there is no more '_' in the $right selector
+          // show the country's image and play the anthem
           var countries = $("#countries");
           var imageCountry = $("<img>");
 
@@ -58,14 +71,14 @@
           imageCountry.addClass("country-image");
 
           // Each imageCountry will be given a src link to the country image
-          imageCountry.attr("src", "assets/images/" + image);
+          imageCountry.attr("src", "assets/images/" + this.image);
 
           // Lastly, each country image (with all it classes and attributes) will get added to the page.
           countries.append(imageCountry);
 
           // play music
           var audioElement = document.createElement("audio");
-          audioElement.setAttribute("src", "assets/sounds/" + countryAnthem);
+          audioElement.setAttribute("src", "assets/sounds/" + this.countryAnthem);
           audioElement.play();
 
           this.complete = true;
